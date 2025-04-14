ff3d822817bbc50b255c45dfa0e662142531d29e
zaiyu zhang <144494635+laonaizuanbeiwo1@users.noreply.github.com>
2025-04-13 21:50:18 -0700

    456

--- src/main/java/com/demo/HelloWorld.java
+++ src/main/java/com/demo/HelloWorld.java
@@ -2,6 +2,6 @@ package com.demo;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello from Java + Docker + GitHub Actions!");
+        System.out.println("Hello from Dockerized Java App!");
     }
 }
