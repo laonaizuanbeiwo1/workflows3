2e0b8f89d95ca2b0187a09654176c33129ab5d8e
zaiyu zhang <144494635+laonaizuanbeiwo1@users.noreply.github.com>
2025-04-13 21:39:38 -0700

    123

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM openjdk:8-jdk-alpine
-COPY app/build/libs/app.jar app.jar
+COPY build/libs/*.jar /app.jar
 ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/demo/HelloWorld.java
+++ src/main/java/com/demo/HelloWorld.java
@@ -1,3 +1,5 @@
+package com.demo;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello from Java + Docker + GitHub Actions!");
